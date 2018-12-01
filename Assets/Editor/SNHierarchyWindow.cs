313017caaa80f4df9fba52762c0a395f5ecae92f
KylaPrograms <32083631+KylaPrograms@users.noreply.github.com>
2018-11-30 18:23:12 -0800

    Editor script folder with SceneNode Hierarchy editor window script
    To use SceneNode Hierarchy editor thing: in Unity, go to Window and look for the option SceneNode Hierarchy. It should bring up a window. The window must be open to show an active preview of the SceneNode Hierarchy.

---
+++ Assets/Editor/SNHierarchyWindow.cs
@@ -0,0 +1,49 @@
+﻿// Kyla NeSmith
+// Last edited: Nov. 30, 2018
+
+using UnityEngine;
+using UnityEditor;
+
+
+// https://www.youtube.com/watch?v=491TSNwXTIg
+// https://answers.unity.com/questions/38783/repainting-an-editorwindow-when-its-not-the-curren.html
+// https://docs.unity3d.com/ScriptReference/EditorGUILayout.Toggle.html
+// https://docs.unity3d.com/ScriptReference/EditorGUI.BeginDisabledGroup.html
+// https://www.raywenderlich.com/939-extend-the-unity3d-editor
+public class SNHierarchyWindow : EditorWindow
+{
+    [SerializeField] public static SceneNode root;
+
+    [SerializeField] private static bool preview = true;
+
+    [MenuItem("Window/SceneNode Hierarchy")]
+    public static void ShowWindow()
+    {
+        GetWindow<SNHierarchyWindow>("SN Hierarchy");
+    }
+
+    private void OnGUI()
+    {
+        preview = EditorGUILayout.Toggle("Active Preview", preview);
+
+        EditorGUILayout.BeginHorizontal();
+        GUILayout.Label("The Root", EditorStyles.boldLabel);
+        root = EditorGUILayout.ObjectField(root, typeof(SceneNode), true) as SceneNode;
+        EditorGUILayout.EndHorizontal();
+    }
+
+    private void Update()
+    {
+        ShowXForm();
+    }
+
+    public static void ShowXForm()
+    {
+        if (root != null && preview)
+        {
+            SceneNode tmp = root;
+            Matrix4x4 i = Matrix4x4.identity;
+            tmp.CompositeXform(ref i);
+        }
+    }
+}
