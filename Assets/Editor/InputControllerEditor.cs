40d5f993450915e5724ae5c86410da7f8a094869
KylaPrograms <32083631+KylaPrograms@users.noreply.github.com>
2018-11-30 20:03:37 -0800

    Finished InputControllerEditor

--- Assets/Editor/InputControllerEditor.cs
+++ Assets/Editor/InputControllerEditor.cs
@@ -1,24 +1,27 @@
-﻿using UnityEngine;
+﻿// Kyla NeSmith
+// last edited: Nov. 30, 2018
+// Description: customer editor UI
+
+using UnityEngine;
 using UnityEditor;
 
 // https://answers.unity.com/questions/1085035/how-can-i-create-a-enum-like-as-component-light.html
+// https://answers.unity.com/questions/461370/displaying-tooltips-hint-for-variables-in-inspecto.html
 
 [CustomEditor(typeof(InputController))]
 public class InputControllerEditor : Editor
 {
-    [Tooltip("Selecting Controller allows Keyboard inputs as well as Controller inputs")]
-    [SerializeField] protected ControlMode mode = ControlMode.Controller;
-
     public override void OnInspectorGUI()
     {
         InputController ic = (InputController)target;
         
-        mode = (ControlMode)EditorGUILayout.EnumPopup("Control Mode", mode);
-        ic.controlMode = mode;
+        ic.controlMode = (ControlMode)EditorGUILayout.EnumPopup(
+            new GUIContent("Control Mode", "Selecting Controller still allows Keyboard inputs"), ic.controlMode);
 
-        if(mode == ControlMode.Controller)
+        if(ic.controlMode == ControlMode.Controller)
         {
-            ic.controllerType = (ControllerType)EditorGUILayout.EnumPopup("Controller Type", ic.controllerType);
+            ic.controllerType = (ControllerType)EditorGUILayout.EnumPopup(
+                new GUIContent("Controller Type", "Why are you looking at me? I have nothing to say!"), ic.controllerType);
         }
         
     }
